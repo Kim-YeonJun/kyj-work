84fd5f2a57792c954dcc05caf0eefad2e1cd0158
Kim-YeonJun <whaql87@naver.com>
2022-05-09 09:36:56 +0900

    project1th update0509

--- reactexam1/src/MyFooter.js
+++
@@ -1,5 +0,0 @@
-const MyHeader = () => {
-    return <footer>풋터</footer>
-};
-
-export default MyHeader;
--- reactexam1/src/testindex.js
+++
@@ -1,6 +0,0 @@
-const calc = require("./calc");
-
-// console.log(calc);
-
-console.log(calc.add(1, 2));
-console.log(calc.sub(4, 2));	
